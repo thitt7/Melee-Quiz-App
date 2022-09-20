20d35fde8a9e8c50a015f2227ddc23cc3600558e
Tristan <thitt7@gmail.com>
2022-09-20 02:43:14 -0500

    fixed score calc, added video question support

--- src/Components/videoQuestion.js
+++ src/Components/videoQuestion.js
@@ -7,10 +7,6 @@ function videoQuestion () {
 
 
 
-  function YouTubeGetID(url){
-    url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-    return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_\-]/i)[0] : url[0];
- }
 }
 
 export default videoQuestion
